3973c80969c1f5af1eca2199e4692b89396fa0fd
geilehur <haasi@grape.at>
2013-02-02 13:31:55 +0100

    Update at/ac/tuwien/mobile/ReaderInterface.java
    remove getATR()

--- at/ac/tuwien/mobile/ReaderInterface.java
+++ at/ac/tuwien/mobile/ReaderInterface.java
@@ -3,7 +3,7 @@ package mobile;
 public interface ReaderInterface {
 
 	
-	//open(), close(), getATR, transeive(), getName(), isCardPresent()
+	//open(), close(), transeive(), getName(), isCardPresent()
 	
 	/**
 	 * starts the reader, which is now waiting for cards to read
@@ -11,20 +11,21 @@ public interface ReaderInterface {
 	public void open();
 	
 	/**
-	 * closes every open connection from the reader and itself as well 
+	 * closes every open connection from the reader and
+	 * the applicatiion as well
 	 */
 	public void close();
 	
 	/**
 	 * @return  true - if there is an open connection to a valid card
-	 * 			false - no or invalid card found
+	 * 	    false - no or invalid card found
 	 */
 	public boolean isCardPresent();
 	
 	/**
 	 * renamed "transeive()" function from instructions
-	 * @param command byte array containing a valid apdu command
-	 * @return response from card to apdu sent
+	 * @param command byte array containing a apdu command
+	 * @return response from card
 	 */
 	public byte[] sendAPDUCommand(byte[] command);
 	
@@ -33,11 +34,6 @@ public interface ReaderInterface {
 	 */
 	public String getName();
 	
-	/**
-	 * @return tbd...
-	 */
-	public byte[] getATR();	
-
 	
 	
 }
